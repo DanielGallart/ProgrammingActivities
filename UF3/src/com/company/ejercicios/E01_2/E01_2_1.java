832e46cb39f6b464871657d93e8f1a269ba860ed
Daniel Gallart <daniel.gallart@enti.cat>
2022-03-18 18:30:47 +0100

    Files exercices. 1 and 2 done

---
+++ UF3/src/com/company/ejercicios/E01_2/E01_2_1.java
@@ -0,0 +1,28 @@
+package com.company.ejercicios.E01_2;
+
+import java.io.File;
+
+public class E01_2_1 {
+    public static void main(String[] args) {
+        listarDescargas();
+    }
+
+    private static void listarDescargas(){
+        String home = System.getProperty("user.home");
+        String rutaCompleta = home + File.separator + "Downloads";
+        File descargas = new File(rutaCompleta);
+        try{
+            File[] elementosDescargas = descargas.listFiles();
+            for (File f: elementosDescargas) {
+                if(f.isDirectory()){
+                    System.out.println("El elemento " + f.getName() + " es un directorio.");
+                }
+                else if(f.isFile()){
+                    System.out.println("El elemento " + f.getName() + " es un documento.");
+                }
+            }
+        }catch(Exception e){
+            System.out.println("ERROR: " + e.getMessage());
+        }
+    }
+}
