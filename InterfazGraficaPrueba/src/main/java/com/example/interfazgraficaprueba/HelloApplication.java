a950df8c99ce389bc8fefac4c0130b81c19c4e2c
Daniel Gallart <daniel.gallart@enti.cat>
2022-04-20 17:12:40 +0200

    Change background color on button click

--- InterfazGraficaPrueba/src/main/java/com/example/interfazgraficaprueba/HelloApplication.java
+++ InterfazGraficaPrueba/src/main/java/com/example/interfazgraficaprueba/HelloApplication.java
@@ -12,9 +12,14 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Daniel Gallart");
         stage.setScene(scene);
         stage.show();
+    }
+
+    @Override
+    public void stop(){
+        
     }
 
     public static void main(String[] args) {
--- InterfazGraficaPrueba/src/main/java/com/example/interfazgraficaprueba/HelloController.java
+++ InterfazGraficaPrueba/src/main/java/com/example/interfazgraficaprueba/HelloController.java
@@ -1,14 +1,46 @@
 package com.example.interfazgraficaprueba;
 
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.Window;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private Button closeButton, redButton, greenButton, blueButton, yellowButton;
+    @FXML
+    private Pane mainPane;
+    @FXML
+    private Label closeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onCloseButtonClick() {
+        Stage stage = (Stage) closeButton.getScene().getWindow();
+        stage.close();
+    }
+
+    @FXML
+    protected void onRedButtonClick(){
+        mainPane.setBackground(new Background(new BackgroundFill(Color.rgb(255,0,0),null,null)));
+    }
+
+    @FXML
+    protected void onBlueButtonClick(){
+        mainPane.setBackground(new Background(new BackgroundFill(Color.rgb(0,0,255),null,null)));
+    }
+
+    @FXML
+    protected void onGreenButtonClick(){
+        mainPane.setBackground(new Background(new BackgroundFill(Color.rgb(0,255,0),null,null)));
+    }
+    @FXML
+    protected void onYellowButtonClick(){
+        mainPane.setBackground(new Background(new BackgroundFill(Color.rgb(255,255,0),null,null)));
     }
 }
